e0096424acbc01529599f83f81e201e8be675ba6
kirk3207 <44026867+kirk3207@users.noreply.github.com>
2018-11-29 14:55:46 +0800

    dijkstra final

--- Floor_Cleaning.cpp
+++ Floor_Cleaning.cpp
@@ -10,14 +10,14 @@ int row, col, battery;
 int BatteryRow, BatteryCol;
 int BatteryNum;
 int step=0;
-char matrix[1000][1000];
-int transMatrix[1000][1000];
-int adj[1000][1000]= {0};
-int d[1000];
-bool visit[1000]= {false};
-bool Clean[1000]= {false};
-int parent[1000];
-string Location[1000];
+char input;
+int transMatrix[2000][2000];
+int adj[20000][20000];
+int d[2000];
+bool visit[2000]= {false};
+bool Clean[2000]= {false};
+int parent[2000];
+string Location[1000000];
 int numOfVertex=0;
 stack<int> trail;
 int RobotRow,RobotCol,RobotBattery;
@@ -35,16 +35,16 @@ void initClean()
 
 void initDistance()
 {
-    for(int i=0; i<1000; i++)
+    for(int i=0; i<2000; i++)
     {
-        d[i]=10000;
+        d[i]=1000000;
     }
     for(int i=1; i<=numOfVertex; i++)
     {
         if(i==BatteryNum);
         else
         {
-            if(adj[BatteryNum][i]!=10000)
+            if(adj[BatteryNum][i]!=1000000)
             {
                 d[i] = 1;
             }
@@ -63,10 +63,11 @@ void Dijkstra(int source)
     d[source]=0;
     visit[source] =true;
     parent[source] = source;
+    cout<<source<<endl;
 
     for(int k=1; k<=numOfVertex; k++)
     {
-        int a=-1, b=-1, Min = 10000;
+        int a=-1, b=-1, Min = 1000000;
         for(int i=1; i<=numOfVertex; i++)
         {
             if(!visit[i] && d[i] < Min)
@@ -148,11 +149,12 @@ void O_goBack()
 
 void initAdj()
 {
-    for(int i=1; i<=numOfVertex; i++)
+    for(int i=0; i<=numOfVertex; i++)
     {
-        for(int j=1; j<=numOfVertex; j++)
+        for(int j=0; j<=numOfVertex; j++)
         {
-            adj[i][j] = 10000;
+            adj[i][j] = 1000000;
+            //cout<<i<<" "<<j<<endl;
         }
     }
     return ;
@@ -160,27 +162,233 @@ void initAdj()
 
 void BuildAdj()
 {
-    for(int i=0; i<=row; i++)
+    for(int i=0; i<row; i++)
     {
-        for(int j=0; j<=col; j++)
+        for(int j=0; j<col; j++)
         {
             if(transMatrix[i][j]!=0)
             {
-                if(transMatrix[i][j+1]!=0)
+                if(i==0||j==0||i==row-1||j==col-1)
                 {
-                    adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                    if(i==0&&j==0) //左上
+                    {
+                        if(transMatrix[i][j+1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1000000;
+                        }
+                        if(transMatrix[i+1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1000000;
+                        }
+                    }
+                    else if(i==0&&j==col-1) //右上
+                    {
+                        if(transMatrix[i][j-1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1000000;
+                        }
+                        if(transMatrix[i+1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1000000;
+                        }
+                    }
+                    else if(i==row-1&&col==0) //左下
+                    {
+                        if(transMatrix[i-1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1000000;
+                        }
+                        if(transMatrix[i][j+1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1000000;
+                        }
+                    }
+                    else if(i==row-1&&j==col-1) //右下
+                    {
+                        if(transMatrix[i-1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1000000;
+                        }
+                        if(transMatrix[i][j-1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1000000;
+                        }
+                    }
+                    else if(i==0)
+                    {
+                        if(transMatrix[i][j+1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1000000;
+                        }
+                        if(transMatrix[i+1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1000000;
+                        }
+                        if(transMatrix[i][j-1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1000000;
+                        }
+                    }
+                    else if(i==row-1)
+                    {
+                        if(transMatrix[i][j-1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j-1]]=1000000;
+                        }
+                        if(transMatrix[i][j+1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1000000;
+                        }
+                        if(transMatrix[i-1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1000000;
+                        }
+                    }
+                    else if(j==0)
+                    {
+                        if(transMatrix[i-1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1000000;
+                        }
+                        if(transMatrix[i][j+1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1000000;
+                        }
+                        if(transMatrix[i+1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1000000;
+                        }
+                    }
+                    else if(j==col-1)
+                    {
+                        if(transMatrix[i-1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i-1][j]]=1000000;
+                        }
+                        if(transMatrix[i+1][j]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i+1][j]]=1000000;
+                        }
+                        if(transMatrix[i][j-1]!=0)
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                        }
+                        else
+                        {
+                            adj[transMatrix[i][j]][transMatrix[i][j+1]]=1000000;
+                        }
+                    }
                 }
-                if(transMatrix[i][j-1]!=0)
+                else
                 {
-                    adj[transMatrix[i][j]][transMatrix[i][j-1]]=1;
-                }
-                if(transMatrix[i+1][j]!=0)
-                {
-                    adj[transMatrix[i][j]][transMatrix[i+1][j]]=1;
-                }
-                if(transMatrix[i-1][j]!=0)
-                {
-                    adj[transMatrix[i][j]][transMatrix[i-1][j]]=1;
+                    if(transMatrix[i][j+1]!=0)
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i][j+1]]=1;
+                    }
+                    else
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i][j+1]]=1000000;
+                    }
+                    if(transMatrix[i][j-1]!=0)
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i][j-1]]=1;
+                    }
+                    else
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i][j-1]]=1000000;
+                    }
+                    if(transMatrix[i+1][j]!=0)
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i+1][j]]=1;
+                    }
+                    else
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i+1][j]]=1000000;
+                    }
+                    if(transMatrix[i-1][j]!=0)
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i-1][j]]=1;
+                    }
+                    else
+                    {
+                        adj[transMatrix[i][j]][transMatrix[i-1][j]]=1000000;
+                    }
                 }
             }
         }
@@ -194,19 +402,20 @@ int main(int argc, char *argv[])
     string location;
     stringstream inlocation;
     stringstream outlocation;
-    inlocation<<"./"<<argv[1]<<"/floor.data";
-    outlocation<<"./"<<argv[1]<<"/final.path";
-    fin.open(inlocation.str());
-    fout.open(outlocation.str());
-    //fin.open("floor.data");
-    //fout.open("final.path",ios::out);
+    //inlocation<<"./"<<argv[1]<<"/floor.data";
+    //outlocation<<"./"<<argv[1]<<"/final.path";
+    //fin.open(inlocation.str());
+    //fout.open(outlocation.str());
+    fin.open("floor.data");
+    fout.open("final.path",ios::out);
     fin>>row>>col>>battery;
+    cout<<"start reading floor.data"<<endl;
     for(int i=0; i<row; i++)
     {
         for(int j=0; j<col; j++)
         {
-            fin>>matrix[i][j];
-            if(matrix[i][j]=='R')
+            fin>>input;
+            if(input=='R')
             {
                 transMatrix[i][j]=++numOfVertex;
                 BatteryNum = numOfVertex;
@@ -218,11 +427,11 @@ int main(int argc, char *argv[])
                 location = s.str();
                 Location[numOfVertex]=location;
             }
-            else if(matrix[i][j]=='1')
+            else if(input=='1')
             {
                 transMatrix[i][j]=0 ;
             }
-            else if(matrix[i][j]=='0')
+            else if(input=='0')
             {
                 transMatrix[i][j]=++numOfVertex;
                 s.str("");
@@ -231,20 +440,28 @@ int main(int argc, char *argv[])
                 location = s.str();
                 Location[numOfVertex]=location;
             }
-            //fout<<matrix[i][j]<<" ";
+            else
+            {
+                j--;
+            }
         }
-        //fout<<endl;
     }
-
+    cout<<"finished reading floor.data"<<endl;
 
     RobotRow = BatteryRow;
     RobotCol = BatteryCol;
     RobotBattery = battery;
     fin.close();
     //matrix finish
+    /*cout<<"Begin initializing Adj"<<endl;
     initAdj();
+    cout<<"End initializing Adj"<<endl;*/
+    cout<<"Begin building Adj"<<endl;
     BuildAdj();
+    cout<<"End building Adj"<<endl;
+    cout<<"Begin Dijkstra"<<endl;
     Dijkstra(BatteryNum);
+    cout<<"finished Dijkstra"<<endl;
     Clean[BatteryNum]=true;
     for(int i=1;i<=numOfVertex;i++)
     {
@@ -266,5 +483,7 @@ int main(int argc, char *argv[])
         }
     }
     fout.close();
+    //cout<<BatteryRow<<","<<BatteryCol<<endl;
+    cout<<BatteryNum<<endl;
     return 0;
 }
